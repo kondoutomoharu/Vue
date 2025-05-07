7e531452c0706474b8fcf86ebfb2d9f4733e0937
HW_PC155 <hw_pc155@HW-PC155noMacBook-Air.local>
2025-05-07 14:14:29 +0900

    add

---
+++ vue_training02/src/App.vue
@@ -0,0 +1,16 @@
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+type Person = {
+  name: string
+  age: number
+}
+
+const personData = ref<Person>({name:"近藤",age:25});
+</script>
+<template>
+  <p>名前: {{ personData.name }}</p>
+  <p>年齢: {{ personData.age }}</p>
+</template>
