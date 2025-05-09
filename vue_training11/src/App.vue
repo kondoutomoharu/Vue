c87a2198033ecbc3c5182ad504d4626bf12d0335
HW_PC155 <hw_pc155@HW-PC155noMacBook-Air.local>
2025-05-09 15:52:00 +0900

    add

---
+++ vue_training11/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted,ref,onUpdated } from "vue";
+import ChildComponent from "./components/ChildComponent.vue";
+  onMounted(()=>{
+    console.log("Hello Vue!")
+  })
+  const booleanValue = ref<boolean>(true)
+  onUpdated(()=>{
+    console.log("値が更新されました。")
+  })
+</script>
+
+<template>
+  <p>親コンポーネントです。</p>
+  <p v-if="booleanValue">表示されました</p>
+  <p v-if="!booleanValue">非表示になりました。</p>
+  <button @click="booleanValue = !booleanValue">ボタン</button>
+  <ChildComponent v-if="booleanValue"/>
+</template>
---
+++ vue_training11/src/components/ChildComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    import { onUnmounted,ref } from 'vue';
+    onUnmounted(()=>{
+        console.log("子コンポーネントが削除されました。")
+    })
+</script>
+
+<template>
+    <div>子コンポーネントです。</div>
+</template>
