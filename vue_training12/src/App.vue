5cac0b26f92d55f3509580d9eef5d0e43ee5b078
HW_PC155 <hw_pc155@HW-PC155noMacBook-Air.local>
2025-05-09 16:29:05 +0900

    add

---
+++ vue_training12/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { useTemplateRef } from 'vue';
+  const videoRef = useTemplateRef<HTMLVideoElement | null>('vioRef')
+
+  const playVideo = ()=>{
+    videoRef.value.play()
+  }
+  const pauseVideo = ()=>{
+    videoRef.value.pause()
+  }
+</script>
+
+<template>
+  <video src="https://videos.pexels.com/video-files/27545637/12164137_1920_1080_60fps.mp4" ref="vioRef"></video>
+
+  <button @click="playVideo">再生</button>
+  <button @click="pauseVideo">停止</button>
+</template>
+
+<style scoped>
+video {
+  width: 100%;
+}
+</style>
