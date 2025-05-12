7ca28ebb08e9a3c379033a34b14e72bd95030616
HW_PC155 <hw_pc155@HW-PC155noMacBook-Air.local>
2025-05-12 11:00:11 +0900

    add

--- vue_training14/src/App.vue
+++ vue_training14/src/App.vue
@@ -1,47 +1,10 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Router 総復習</h1>
+    <nav>
+      <RouterLink to="/">ホーム</RouterLink> |
+      <RouterLink to="/users">ユーザーリスト</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
